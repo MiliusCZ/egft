c80a58bab2fbcfea52b68bad1a382b9cea8e30ec
Milos Turek <Milos Turek>
2016-07-24 12:17:54 +0200

    Components setup

---
+++ src/components/footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export class Footer extends Component {
+    render() {
+        return (
+            <footer className="mdl-mini-footer">
+                <div className="mdl-mini-footer__left-section">
+                    <div className="mdl-logo">(c) 2016</div>
+                    <ul className="mdl-mini-footer__link-list">
+                    </ul>
+                </div>
+            </footer>
+        );
+    }
+}
